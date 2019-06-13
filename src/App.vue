2606f8fecaa66c929a1495371f09220791ef0b00
wangzz <81303012@qq.com>
2019-06-13 15:21:13 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<!-- 1. 模板：html结构 -->
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+  </div>
+</template>
+
+<!-- 2. 行为：处理逻辑 -->
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+
+<!-- 3. 样式：解决样式 -->
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
